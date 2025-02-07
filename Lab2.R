842ca5cbe7ab22fe7d179681c96617ba52d091ff
tec234 <86253216+tec234@users.noreply.github.com>
2025-02-07 14:59:50 -0500

    Add files via upload

---
+++ Lab2.R
@@ -0,0 +1,48 @@
+library("ggplot2")
+library("readr")
+
+## read dataset
+NY_House_Dataset <- read_csv("Courses/Data Analytics/Fall24/assignments/NY-House-Dataset.csv")
+
+dataset <- NY_House_Dataset
+
+ggplot(dataset, aes(x = log10(PROPERTYSQFT), y = log10(PRICE))) +
+  geom_point()
+
+## filter data
+dataset <- dataset[dataset$PRICE<195000000,]
+
+dataset <- dataset[dataset$PROPERTYSQFT!=2184.207862,]
+
+dataset$PROPERTYSQFT[dataset$BROKERTITLE=="Brokered by Douglas Elliman - 575 Madison Ave"][85]
+
+## column names
+names(dataset)
+
+## fit linear model
+lmod <- lm(PRICE~PROPERTYSQFT, data = dataset)
+
+lmod <- lm(log10(PRICE)~log10(PROPERTYSQFT), data = dataset)
+
+## print model output
+summary(lmod)
+
+## scatter plot of 2 variables
+plot(PRICE~PROPERTYSQFT, data = dataset)
+abline(lmod)
+
+plot(log10(PRICE)~log10(PROPERTYSQFT), data = dataset)
+abline(lmod)
+
+## scatter plot of 2 variables
+ggplot(dataset, aes(x = PROPERTYSQFT, y = PRICE)) +
+  geom_point()
+
+ggplot(dataset, aes(x = PROPERTYSQFT, y = PRICE)) +
+  geom_point() +
+  stat_smooth(method = "lm", col="red")summary(model1)
+
+
+ggplot(dataset, aes(x = log10(PROPERTYSQFT), y = log10(PRICE))) +
+  geom_point() +
+  stat_smooth(method = "lm", col="red")
